fd4a2a8fa8f3ef90816c2b7265afe9d85294ec43
Irfandi Rustam <irfandy4hmad@gmail.com>
2022-11-02 22:27:33 +0800

    part 4 - How to create first test

---
+++ tests/my_first_test.spec.js
@@ -0,0 +1,8 @@
+const { test, expect } = require('@playwright/test');
+
+test('My first test', async ({page}) => {
+
+  await page.goto('https://www.google.com');
+  await expect(page).toHaveTitle('Google');
+
+})
