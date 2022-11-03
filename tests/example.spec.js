f432c2373243d189b0e4d8e05036bd44feec8c5e
Irfandi Rustam <irfandy4hmad@gmail.com>
2022-11-03 22:03:22 +0800

    Part 6: How to use Trace Viewer

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,21 +1,43 @@
 // @ts-check
-const { test, expect } = require('@playwright/test');
+const { test, expect } = require("@playwright/test");
 
-test('homepage has Playwright in title and get started link linking to the intro page', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+let context;
+let page;
+test.beforeAll(async ({ browser }) => {
+  context = await browser.newContext();
+  await context.tracing.start({ 
+    screenshots: true, 
+    snapshots: true 
+  });
+  page = await context.newPage();
+});
+
+test.afterAll(async () => {
+  await context.tracing.stop({ path: "test2_trace.zip" });
+});
+
+test("homepage has Playwright", async ({ }) => {
+  // await context.tracing.start({
+  //   snapshots: true,
+  //   screenshots: true
+  // });
+
+  await page.goto("https://playwright.dev/");
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
 
   // create a locator
-  const getStarted = page.getByText('Get Started');
+  const getStarted = page.getByText("Get Started");
 
   // Expect an attribute "to be strictly equal" to the value.
-  await expect(getStarted).toHaveAttribute('href', '/docs/intro');
+  await expect(getStarted).toHaveAttribute("href", "/docs/intro");
 
   // Click the get started link.
   await getStarted.click();
-  
+
   // Expects the URL to contain intro.
   await expect(page).toHaveURL(/.*intro/);
+
+  // await context.tracing.stop({path: 'test1_trace.zip'});
 });
