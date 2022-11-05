0dbe766a95911824b34c2b814e4b2e103d117f1b
Irfandi Rustam <irfandy4hmad@gmail.com>
2022-11-05 21:20:35 +0800

    Part 12: Annotations & Tags

---
+++ tests/annotations_tags.spec.js
@@ -0,0 +1,23 @@
+import { test, expect } from "@playwright/test";
+
+test.skip('Test One', async ({page}) => {
+
+});
+
+test('not yet ready', async ({ page }) => {
+  test.fail();
+});
+
+test.fixme('test to be fixed', async ({ page }) => {
+  // ...
+});
+
+test('slow test', async ({ page }) => {
+  test.slow();
+  // ...
+});
+
+// Tags
+test('Test login page @smoke', async ({ page }) => {
+  // ...
+});
